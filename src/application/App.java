9a27c7c5440e9f43786076bebb95d29a418afafb
Mauricio Lopes <mauriciolweb@gmail.com>
2023-09-28 14:31:01 -0300

    implementando o método de busca por departamento

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,9 @@
 package application;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
@@ -8,8 +11,15 @@ public class App {
        
         SellerDao sellerDaoJDBC = DaoFactory.createSellerDaoJDBC();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDaoJDBC.findById(3);
-        
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        List<Seller> sellers = sellerDaoJDBC.findByDepartment(new Department(1, null));
+        
+        for (Seller seller2 : sellers) {
+            System.out.println(seller2);
+        }
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -67,15 +70,57 @@ public class SellerDaoJDBC implements SellerDao{
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
-
+        
     }
-
+    
     @Override
     public List<Seller> findAll() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findAll'");
     }
     
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                "SELECT seller.*,department.Name as DepName "
+                + "FROM seller INNER JOIN department "
+                + "ON seller.DepartmentId = department.Id "
+                + "WHERE DepartmentId = ? "
+                + "ORDER BY Name");
+
+            st.setInt(1, department.getId());
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();            
+            Map<Integer, Department> map = new HashMap<>();
+
+            while(rs.next()){
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+                
+                if(dep == null){
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+
+            return list;
+        
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
     // métodos auxiliares
     private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
         Seller obj = new Seller();
@@ -94,4 +139,5 @@ public class SellerDaoJDBC implements SellerDao{
         dep.setName(rs.getString("DepName"));
         return dep;
     }
+
 }
