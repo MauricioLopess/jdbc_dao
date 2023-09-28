78208ae2c910a7053b7a8ada7d6134cae9527516
Mauricio Lopes <mauriciolweb@gmail.com>
2023-09-27 22:02:02 -0300

    criação da classe de vendedores

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.time.LocalDateTime;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Department department = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDateTime.now(), 3000.00, department);
         
+        System.out.println(seller);
     }
 }
